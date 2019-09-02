0805cc9354a243600e35bc44f26400517862fc5d
Jack Furey <jfurey14@live.unc.edu>
2019-09-02 03:51:44 -0400

    Novice finished

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,21 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		int totalCust = scan.nextInt();
+		for(int i=0; i<totalCust; i++) {
+
+			String first = scan.next();
+			String last = scan.next();
+			int totalItem = scan.nextInt();
+			double totalCost = 0;
+			for(int j=0; j<totalItem; j++) {
+				double quan = scan.nextInt();
+				String itemName = scan.next();
+				double price = scan.nextDouble();
+				totalCost = totalCost + quan * price;
+			}
+		System.out.println(first.charAt(0) + ". " + last + ": " + totalCost);
+	
+		}
 	}
 }
